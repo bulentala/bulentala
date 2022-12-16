eef852861ba578a29c79e2ef3099a965329bfac5
Bülent Ala <bulentala@gmail.com>
2022-12-16 16:14:43 +0300

    oks

--- app/components/Test.js
+++ app/components/Test.js
@@ -1,47 +1,22 @@
-"use client";
 import React from "react";
-import { useState } from "react";
 
-const Colors = () => {
-  const [color, setColor] = useState("bg-red-500");
-  const [sliderValue, setSliderValue] = useState(0);
-  const [fontSize, setFontSize] = useState(16);
+const data = [
+  { id: 1, date: "01.01.2022 16:45" },
+  { id: 2, date: "02.02.2022 16:45" },
+  { id: 3, date: "03.03.2022 16:45" },
+  { id: 4, date: "04.04.2022 16:45" },
+  { id: 5, date: "05.05.2022 16:45" },
+  { id: 6, date: "06.06.2022 16:45" },
+  { id: 7, date: "07.07.2022 16:45" },
+  { id: 8, date: "08.08.2022 16:45" },
+  { id: 9, date: "09.09.2022 16:45" },
+  { id: 10, date: "10.10.2022 16:45" },
+  { id: 11, date: "11.11.2022 16:45" },
+  { id: 12, date: "12.12.2022 16:45" },
+];
 
-  const changeColor = (newColor) => {
-    setColor(newColor);
-  };
-
-  const handleSliderChange = (event) => {
-    setSliderValue(event.target.value);
-    setFontSize(event.target.value);
-  };
-
-  return (
-    <div>
-      <div className={color} style={{ fontSize: `${fontSize}px` }}>
-        Text inside the div
-      </div>
-      <button
-        className='rounded bg-red-500 py-2 px-4 font-bold text-white hover:bg-red-700'
-        onClick={() => changeColor("bg-red-500")}
-      >
-        Red button
-      </button>
-      <button
-        className='rounded bg-black py-2 px-4 font-bold text-white hover:bg-gray-700'
-        onClick={() => changeColor("bg-black")}
-      >
-        Black button
-      </button>
-      <input
-        type='range'
-        min='16'
-        max='48'
-        value={sliderValue}
-        onChange={handleSliderChange}
-      />
-    </div>
-  );
+const Test = () => {
+  return <div>Test Component</div>;
 };
 
-export default Colors;
+export default Test;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
 import Test from "./components/Test";
-import Test2 from "./components/Test2";
+
 const AppIndexPage = () => {
   return (
     <>
       <Test />
-      <Test2 />
     </>
   );
 };
