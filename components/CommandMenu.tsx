76fc1223d16a781dff6597e9f0a4f45d8b4af241
Bulent alae <bulentala@gmail.com>
2024-02-10 13:56:29 +0300

    ok

--- components/CommandMenu.tsx
+++ components/CommandMenu.tsx
@@ -48,19 +48,40 @@ const CommandMenu = () => {
 
   return (
     <>
-      <Button
-        variant='outline'
-        className={cn(
-          "relative h-8 w-full justify-start rounded-[0.5rem] bg-background text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 lg:w-64"
-        )}
-        onClick={() => setOpen(true)}
-      >
-        <span className='hidden lg:inline-flex'>Search documentation...</span>
-        <span className='inline-flex lg:hidden'>Search...</span>
-        <kbd className='pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex'>
-          <span className='text-xs'>⌘</span>K
+      <div onClick={() => setOpen(true)}>
+        <kbd>
+          {" "}
+          <svg
+            stroke-width='1.5'
+            viewBox='0 0 24 24'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5'
+          >
+            <path
+              d='M3 5H11'
+              stroke='currentColor'
+              stroke-width='1.5'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+            ></path>
+            <path
+              d='M3 12H16'
+              stroke='currentColor'
+              stroke-width='1.5'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+            ></path>
+            <path
+              d='M3 19H21'
+              stroke='currentColor'
+              stroke-width='1.5'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+            ></path>
+          </svg>
         </kbd>
-      </Button>
+      </div>
       <CommandDialog open={open} onOpenChange={setOpen}>
         <CommandInput placeholder='Type a command or search...' />
         <CommandList>
