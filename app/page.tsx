96dd912ba555f6026f0d50b4197f9e30a677ca83
Bulent alae <bulentala@gmail.com>
2024-10-18 17:01:12 +0300

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,75 @@
-import Image from "next/image";
+export default function HomePage() {
+  const colors = [
+    "grey",
+    "brown",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "pink",
+    "red",
+  ];
 
-export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="mx-auto max-w-2xl min-h-screen">
+      <div className="text-center">
+        <div>A|A</div>
+        <div className="rounded-full w-12 h-12 items-center justify-center flex mx-auto bg-notion-bg-purple" />
+        <div>May the force be with you</div>
+        <div>Işık Ülkesine Dair</div>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      {Array.from({ length: 15 }).map((_, index) => {
+        // Renkleri seçerken tekrar etmemesi için bir kopya dizi kullanıyoruz
+        const remainingColors = [...colors];
+
+        const color1 = remainingColors.splice(
+          Math.floor(Math.random() * remainingColors.length),
+          1
+        )[0];
+        const color2 = remainingColors.splice(
+          Math.floor(Math.random() * remainingColors.length),
+          1
+        )[0];
+        const color3 = remainingColors.splice(
+          Math.floor(Math.random() * remainingColors.length),
+          1
+        )[0];
+        const color4 = remainingColors.splice(
+          Math.floor(Math.random() * remainingColors.length),
+          1
+        )[0];
+
+        return (
+          <div
+            key={index}
+            className={`p-3 group cursor-pointer hover:bg-notion-bg-${color1} hover:shadow-sm shadow-notion-bg-${color1}/50`}
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <div className="flex items-center">
+              <div
+                className={`pr-2 text-notion-grey group-hover:text-notion-${color2}`}
+              >
+                2024
+              </div>
+              <div
+                className={`line-clamp-1 font-medium group-hover:text-notion-${color3}`}
+              >
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </div>
+            </div>
+            <div
+              className={`text-notion-grey group-hover:text-notion-${color4}`}
+            >
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Cupiditate, et debitis harum aliquid pariatur perferendis nemo
+              adipisci quidem fugit nulla?
+            </div>
+          </div>
+        );
+      })}
+
+      <div className="text-center">footer</div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,25 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'notion-default': '#37352F',
+        'notion-grey': '#787774',
+        'notion-brown': '#9F6B53',
+        'notion-orange': '#D9730D',
+        'notion-yellow': '#CB912F',
+        'notion-green': '#448361',
+        'notion-blue': '#337EA9',
+        'notion-purple': '#9065B0',
+        'notion-pink': '#C14C8A',
+        'notion-red': '#D44C47',
+        'notion-bg-grey': '#F1F1EF',
+        'notion-bg-brown': '#F4EEEE',
+        'notion-bg-orange': '#FAEBDD',
+        'notion-bg-yellow': '#FBF3DB',
+        'notion-bg-green': '#EDF3EC',
+        'notion-bg-blue': '#E7F3F8',
+        'notion-bg-purple': '#F6F3F9',
+        'notion-bg-pink': '#FAF1F5',
+        'notion-bg-red': '#FDEBEC'
       },
     },
   },
